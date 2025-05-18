89318756213bd95f4530b6470874312e228314e2
shuvo794 <shuvohossain794@gmail.com>
2025-05-18 18:12:27 +0600

    Refactor code structure for improved readability and maintainability

--- components/NavbarSection.tsx
+++ components/NavbarSection.tsx
@@ -103,7 +103,7 @@ export default function NavbarSection() {
       >
         <div className="max-w-[1200px] mx-auto flex justify-between items-center p-4">
           <Link href="/" className="flex items-center gap-2">
-            <Image src="/RAMSNew.png" alt="Logo" width={50} height={70} />
+            <Image src="/RAMSLo11go.png" alt="Logo" width={100} height={100} />
           </Link>
 
           <NavigationMenu className="hidden lg:flex">
