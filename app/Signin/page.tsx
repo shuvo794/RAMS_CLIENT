9d8bf2d26ddd63bfa02128cfd58874990f52a89a
shuvo794 <shuvohossain794@gmail.com>
2025-05-18 18:45:16 +0600

    refactor: reorder Back Button for improved structure and readability

--- app/Signin/page.tsx
+++ app/Signin/page.tsx
@@ -22,16 +22,16 @@ const page = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      {/* Back Button */}
-      <button
-        onClick={() => window.history.back()}
-        className="absolute top-4 left-4 text-white bg-black/50 hover:bg-black/70 px-4 py-2 rounded"
-      >
-        ← Back
-      </button>
-
       <div className="flex flex-col md:flex-row w-full max-w-6xl rounded-2xl shadow-2xl overflow-hidden bg-white">
         {/* Left Section */}
+
+        {/* Back Button */}
+        <button
+          onClick={() => window.history.back()}
+          className="absolute top-4 left-4 text-white bg-black/50 hover:bg-black/70 px-4 py-2 rounded"
+        >
+          ← Back
+        </button>
         <Card
           className="flex flex-col flex-1 items-center justify-center border-0 rounded-none p-12"
           style={{
